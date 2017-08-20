9ae82bffc4c6bde73e837ec00d98a81c389d7d75
Sachin Chimmalgi <sachin@bigphi.com>
2017-08-20 23:09:41 +0530

    Added function to calculate Average Temp.. Need to test it..

--- USB_Ver_-_3_New/USB_Ver_-_3_New.ino
+++ USB_Ver_-_3_New/USB_Ver_-_3_New.ino
@@ -24,6 +24,7 @@ unsigned long Time = 0;
 SoftwareSerial mySerial(2, 6);
 
 OneWire  ds(A0);  // on pin 8 (a 4.7K resistor is necessary)
+OneWire  ds1(A1);
 
 File myFile;
 
@@ -321,6 +322,11 @@ void LogRFIDCard(String PrintToFile){
   wdt_reset();
 }
 
+float AverageTemp(){    //Lets try this tomorrow!!
+  return ((ReadTemperature() + ReadTemperature1())/2);
+}
+
+
 float ReadTemperature(){
   if ( !ds.search(addr)) {
     ds.reset_search();
@@ -352,6 +358,57 @@ float ReadTemperature(){
 
   for ( i = 0; i < 9; i++) {           // we need 9 bytes
     data[i] = ds.read();
+  }
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  return celsius;
+}
+
+float ReadTemperature1(){
+  if ( !ds1.search(addr)) {
+    ds1.reset_search();
+    delay(50);
+//    return;
+  }
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      type_s = 1;
+      break;
+    case 0x28:
+      type_s = 0;
+      break;
+    case 0x22:
+      type_s = 0;
+      break;
+    default:
+      break;
+  } 
+
+  ds1.reset();
+  ds1.select(addr);
+  ds1.write(0x44, 1);        // start conversion, with parasite power on at the end
+  delay(1000);              //DONT REMOVE THIS
+  present = ds1.reset();
+  ds1.select(addr);    
+  ds1.write(0xBE);         // Read Scratchpad
+
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds1.read();
   }
   int16_t raw = (data[1] << 8) | data[0];
   if (type_s) {
